40de50dfb7c68895d691811d5410d8c3a6bef11e
ZOMini <zarikacr@gmail.com>
2022-07-21 20:17:59 +0300

    v 2.5

--- avia/avia/settings.py
+++ avia/avia/settings.py
@@ -78,7 +78,18 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT')
     }
-} 
+}
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
